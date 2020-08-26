a1f4bc6e4f05c251568669a5f7c3b3f50fc50b18
greenh <violetacastrogutierrez@gmail.com>
2020-08-26 21:00:05 +0000

    set up controllers to handle authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      post 'authenticate', to: 'authentication#authenticate'
       resources :trips, only: [:index]
     end
   end
