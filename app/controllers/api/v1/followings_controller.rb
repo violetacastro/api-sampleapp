da09582817be2a8eac1747e76b34d1f377100724
greenh <violetacastrogutierrez@gmail.com>
2021-01-07 21:26:50 +0000

    api returns a json result to react

--- app/controllers/api/v1/followings_controller.rb
+++ app/controllers/api/v1/followings_controller.rb
@@ -4,8 +4,16 @@ class Api::V1::FollowingsController < Api::ApplicationController
     following = Following.new
     following.follower_id = @current_api_user.id
     following.author_id = params[:author_id]
-    following.save
-    binding.pry
+
+    if following.save
+      render json: {
+        status: 200
+      }.to_json
+    else
+      render json: {
+        status: 400
+      }.to_json
+    end
   end
 
 end
