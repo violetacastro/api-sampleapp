877fa2a06410e4fd689aa479fef4bf9fd69dd16a
greenh <violetacastrogutierrez@gmail.com>
2021-01-20 17:13:06 +0000

    add error and message for all conditions in followings controller

--- app/controllers/api/v1/followings_controller.rb
+++ app/controllers/api/v1/followings_controller.rb
@@ -8,13 +8,14 @@ class Api::V1::FollowingsController < Api::ApplicationController
     following_exist = Following.find_by(follower_id: @current_api_user.id,
                                         author_id: params[:author_id])
     if following_exist
-      render json: { message: "following exist already" }, status: 401
+      render json: { message: "following exist already", error: true }, status: 400
     elsif following.follower_id == following.author_id
-      render json: { message: "cant follow yourself" }, status: 401
+      render json: { message: "cant follow yourself", error: true }, status: 400
     elsif following.save
       render json: { message: "successful following" }, status: 201
     else
-      render json: { mensage: "other" }, status: 401
+      render json: { message: "Oops we could not follow this user for you. Please try again soon",
+                     errors: following.errors.full_messages, error: true }, status: 400
     end
 
   end
