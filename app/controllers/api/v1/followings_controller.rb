e506b832d168b4c14a6521b4aa2c2e1e9cd58bac
greenh <violetacastrogutierrez@gmail.com>
2021-01-25 21:26:35 +0000

    check if following exists

--- app/controllers/api/v1/followings_controller.rb
+++ app/controllers/api/v1/followings_controller.rb
@@ -8,15 +8,42 @@ class Api::V1::FollowingsController < Api::ApplicationController
     following_exist = Following.find_by(follower_id: @current_api_user.id,
                                         author_id: params[:author_id])
     if following_exist
-      render json: {}, status: 401
+      render json: { message: "following exist already", error: true }, status: 400
     elsif following.follower_id == following.author_id
-      render json: {}, status: 401
+      render json: { message: "cant follow yourself", error: true }, status: 400
     elsif following.save
-      render json: {}, status: 201
+      render json: { message: "successful following" }, status: 201
     else
-      render json: {}, status: 401
+      render json: { message: "Oops we could not follow this user for you. Please try again soon",
+                     errors: following.errors.full_messages, error: true }, status: 400
     end
+  end
 
+  # check if there's an existing following already wih the current user and
+  # the author_id
+  def get_following
+    # check if theres an author_id
+    author_id = params[:author_id]
+    # if the following exist return the following, else will be null
+    if !author_id
+      render json: { error: true, message: "author_id not found" }, status: 400
+    else
+      following = Following.find_by(follower_id: @current_api_user.id,
+                                 author_id: author_id)
+      render json: { following: following }, status: 200
+    end
+  end
+
+  def destroy
+
+      following = Following.find_by(id: params[:id])
+
+      if !following
+        render json: { error: true, message: "following not found" }, status: 400
+      else
+        following.destroy
+        render json: { message: "successful unfollow" }, status: 202
+      end
   end
 
 end
